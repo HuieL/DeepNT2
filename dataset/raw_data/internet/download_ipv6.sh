024fa25969789a4078dfc7538a908f7707ec32b0
HuieL <82495316+HuieL@users.noreply.github.com>
2024-07-15 01:47:23 +0800

    Update download_ipv6.sh

--- dataset/raw_data/internet/download_ipv6.sh
+++ dataset/raw_data/internet/download_ipv6.sh
@@ -1,3 +1,3 @@
-perl scripts/gdown.pl https://drive.google.com/file/d/1nJdljy468roUcKLbVwWUhMs7teirah75/view?usp=sharing ipv6_probe.zip
+perl scripts/gdown.pl https://drive.google.com/file/d/1BX8KSqytkYn1GOkTw9U7m1bSzd4hEJAA/view?usp=sharing ipv6_probe.zip
 unzip ipv6_probe.zip
 rm ipv6_probe.zip
