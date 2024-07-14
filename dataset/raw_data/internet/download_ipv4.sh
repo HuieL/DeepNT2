67cbff345e87031530651a5e39ffaa0985f10a5d
HuieL <82495316+HuieL@users.noreply.github.com>
2024-07-14 22:12:30 +0800

    Update download_ipv4.sh

--- dataset/raw_data/internet/download_ipv4.sh
+++ dataset/raw_data/internet/download_ipv4.sh
@@ -1,3 +1,3 @@
-perl scripts/gdown.pl https://drive.google.com/file/d/1nJdljy468roUcKLbVwWUhMs7teirah75/view?usp=sharing ipv6_probe.zip
-unzip ipv6_probe.zip
-rm ipv6_probe.zip
+perl scripts/gdown.pl https://drive.google.com/file/d/1nJdljy468roUcKLbVwWUhMs7teirah75/view?usp=sharing ipv4_probe.zip
+unzip ipv4_probe.zip
+rm ipv4_probe.zip
