16a5675b0c4856259f86ee7696a9fe5159455c17
HuieL <82495316+HuieL@users.noreply.github.com>
2024-07-15 22:06:47 +0800

    Update internet_graph.py

--- src/utils/internet_graph.py
+++ src/utils/internet_graph.py
@@ -246,9 +246,10 @@ def process_warts_files(input_dir, output_dir, max_records=None, num_walks=10, w
 
 def main():
     parser = argparse.ArgumentParser(description="Convert WARTS files to PyG graphs")
-    parser.add_argument("--input", type=str, default="./dataset/raw_data/internet/ipv6_probe",
+    parser.add_argument("--dataset_name", type=str, choices=["ipv4_probe", "ipv6_probe"])
+    parser.add_argument("--input", type=str, default=f"./dataset/raw_data/internet/{dataset_name}",
                         help="Input folder containing WARTS files")
-    parser.add_argument("--output", type=str, default="./dataset/processed_data/internet/ipv6_probe",
+    parser.add_argument("--output", type=str, default=f"./dataset/processed_data/internet/{dataset_name}",
                         help="Output folder for processed PyG graphs")
     parser.add_argument("--max_records", type=int, default=1000,
                         help="Maximum number of records to process per file (default: all)")
