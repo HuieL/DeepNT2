19041ea4b0e1b617aa4d2a66269795c33d70ff30
HuieL <82495316+HuieL@users.noreply.github.com>
2024-08-18 00:04:01 -0400

    Update metrics.py

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -2,3 +2,6 @@ import torch
 
 def mape_calculation(output, target):
     return torch.abs((target - output) / target) * 100
+
+def mse_calculation(output, target):
+    return torch.mean((target - output) ** 2)
