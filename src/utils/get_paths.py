b3026e944d1e4367ffc480914605d90d2f36a672
HuieL <82495316+HuieL@users.noreply.github.com>
2024-08-20 05:16:57 -0400

    Update get_paths.py

--- src/utils/get_paths.py
+++ src/utils/get_paths.py
@@ -1,6 +1,7 @@
 from collections import deque
 import random
 import torch
+import networkx as nx
 
 
 # Build adjacent matrix with edge_index
@@ -19,55 +20,79 @@ def edge_masked(edge_index, num_nodes, topology_error_rate, seed=None):
     num_edges = edge_index.shape[1]
     num_errors = int(num_edges * topology_error_rate)
     
-    masked_edge_index = edge_index.clone()
-    edges_to_replace = random.sample(range(num_edges), num_errors)
+    G = nx.Graph()
+    G.add_nodes_from(range(num_nodes))
+    edge_list = edge_index.t().tolist()
+    G.add_edges_from(edge_list)
     
-    for edge_idx in edges_to_replace:
-        while True:
-            new_source = random.randint(0, num_nodes - 1)
-            new_target = random.randint(0, num_nodes - 1)
-            
-            if new_source != new_target and not edge_exists(masked_edge_index, new_source, new_target):
-                masked_edge_index[:, edge_idx] = torch.tensor([new_source, new_target])
-                break
+    edges_to_remove = random.sample(list(G.edges()), num_errors)
+    G.remove_edges_from(edges_to_remove)
     
-    return masked_edge_index
-
-def edge_exists(edge_index, source, target):
-    return ((edge_index[0] == source) & (edge_index[1] == target)).any() or \
-           ((edge_index[0] == target) & (edge_index[1] == source)).any()
+    for _ in range(num_errors):
+        components = list(nx.connected_components(G))
+        if len(components) == 1:
+            while True:
+                new_source = random.randint(0, num_nodes - 1)
+                new_target = random.randint(0, num_nodes - 1)
+                if new_source != new_target and not G.has_edge(new_source, new_target):
+                    G.add_edge(new_source, new_target)
+                    break
+        else:
+            comp1, comp2 = random.sample(components, 2)
+            new_source = random.choice(list(comp1))
+            new_target = random.choice(list(comp2))
+            G.add_edge(new_source, new_target)
+    
+    new_edge_list = list(G.edges())
+    new_edge_index = torch.tensor(new_edge_list, dtype=torch.long).t()
+    
+    return new_edge_index
 
 # Randomly sample paths
-def random_sample_paths(adj, start, goal, num_paths=2, max_attempts=200, max_depth=10):
-    all_paths = set()
+def random_sample_paths(G, start, goal, num_paths, max_depth):
+    all_paths = []
+    for path in nx.shortest_simple_paths(G, start, goal):
+        if len(path) > max_depth:
+            break
+        all_paths.append(path)
+        if len(all_paths) >= num_paths:
+            break
 
-    def bfs_paths(adj, start, goal, max_attempts, max_depth):
-        queue = deque([(start, [start])])
-        attempts = 0
+    if len(all_paths) < num_paths:
+        return all_paths
+    return random.sample(all_paths, num_paths)
 
-        while queue and attempts < max_attempts:
-            node, path = queue.popleft()
+def batch_random_sample_paths(adj, starts, goals, num_paths, max_depth=10):
+    batch_size = starts.shape[0]
+    G = nx.from_numpy_array(adj.cpu().numpy())
+    
+    all_paths = []
+    path_lengths = []
+    
+    for i in range(batch_size):
+        start, goal = starts[i].item(), goals[i].item()
+        paths = random_sample_paths(G, start, goal, num_paths, max_depth)
 
-            if node == goal:
-                all_paths.add(tuple(path))
-                attempts += 1
-                if len(all_paths) >= num_paths:
-                    break
-                continue
+        padded_paths = []
+        lengths = []
 
-            if len(path) >= max_depth:
-                continue
+        while len(paths) < num_paths:
+            paths.append([start, goal])  
 
-            neighbors = torch.nonzero(adj[node]).squeeze().tolist()
-            if isinstance(neighbors, int):
-                neighbors = [neighbors]
-            for neighbor in neighbors:
-                if neighbor not in path:
-                    new_path = path + [neighbor]
-                    queue.append((neighbor, new_path))
-        return list(all_paths)
-
-    generated_paths = bfs_paths(adj, start, goal, max_attempts, max_depth)
-    if len(generated_paths) < num_paths:
-        num_paths = len(generated_paths)
-    return random.sample(generated_paths, num_paths)
+        for path in paths:
+            if len(path) < max_depth:
+                padded_path = path + [path[-1]] * (max_depth - len(path))
+                length = len(path)
+            else:
+                padded_path = path[:max_depth]
+                length = max_depth
+            padded_paths.append(padded_path)
+            lengths.append(length)
+        
+        while len(padded_paths) < num_paths:
+            padded_paths.append(padded_paths[-1] if padded_paths else [start] * max_depth)
+        
+        all_paths.append(padded_paths)
+        path_lengths.append(lengths)
+    
+    return torch.LongTensor(all_paths).to(starts.device), torch.LongTensor(path_lengths).to(starts.device)
